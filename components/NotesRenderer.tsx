fc2956cc83e56fd11f07f806dd5ce93ef2d6d2b5
Sami Osman <samisosman52@gmail.com>
2026-03-16 18:50:08 -0400

    Render math properly in notes

--- components/NotesRenderer.tsx
+++ components/NotesRenderer.tsx
@@ -1,11 +1,19 @@
 "use client";
 import ReactMarkdown from "react-markdown";
 import remarkGfm from "remark-gfm";
+import remarkMath from "remark-math";
+import rehypeKatex from "rehype-katex";
+import "katex/dist/katex.min.css";
 
 export default function NotesRenderer({ notes }: { notes: string }) {
   return (
     <div className="notes-content">
-      <ReactMarkdown remarkPlugins={[remarkGfm]}>{notes}</ReactMarkdown>
+      <ReactMarkdown
+        remarkPlugins={[remarkGfm, remarkMath]}
+        rehypePlugins={[rehypeKatex]}
+      >
+        {notes}
+      </ReactMarkdown>
     </div>
   );
 }
